7f18399fe30c69b15ca4fca7de2b1e21df76163b
Matheus Jacob <matheusbendel@hotmail.com>
2021-08-19 02:23:25 -0300

    update(bot): play is working

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json')
-const yts = require("yt-search");
 const play = require("./play");
+let express = require('express');
+global.app = express();
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
 
 client.login(config.token);
 
 client.on("ready", async () => {
-  args
-  let status = play();
 
   console.log('ta piscando, ta gravando !');
 })
@@ -26,44 +27,99 @@ client.on("message", async (message) => {
       message.channel.send('> Olá miguxo')
     if (message.content == `${config.prefix} tudo bem?`)
       message.channel.send('> Estou ótima! 🧏‍♀️ e você ? 🤸‍♀️')
-    if (message.content == `${config.prefix} toca`) {
-      const { voice } = message.member
-      let args = message.content.substring(PREFIX.length).split("zoe toca ")
-      
-      // console.log(voice.channelId);
-
-      if(!voice.channelID){
-        message.reply('You must be in a voice channel !')
-        return
-      }
-
-      client.channels.fetch(voice.channelID).then(channel => {
-        if (!channel) return console.error("The channel does not exist!");
-        channel.join().then(connection => {
-            // Yay, it worked!
-            console.log("Successfully connected.");
-        }).catch(e => {
-            // Oh no, it errored! Let's log it to console :slight_smile:
-            console.error(e);
-        });
-    })
 
 
+    const { voice } = message.member
+    let args = message.content.substring(config.prefix.length).split(' ')
+    let command = message.content.substring(`$(config.prefix.length) play`).split(" ")[1]
 
-      // const { voice } = message.member
-      // console.log(message.member.voice.channelId)
+    client.channels.fetch(voice.channelID).then(channel => {
+      if (!channel) return console.error("The channel does not exist!");
+      let connection = channel.join().then(async(connection) => {
+        // Yay, it worked!
+        console.log("Successfully connected.");
 
-      // if(!voice.channelId){
-      //   message.reply('You must be in a voice channel !')
-      //   return
-      // }
+    // console.log(voice.channelId);
 
-      // else{
+    // if(!voice.channelID){
+    //   message.reply('You must be in a voice channel !')
+    //   return
+    // }
+    // console.log(message);
+    // args = message.content.slice(config.prefix.length).split(/ +/);
 
-      // }
 
+    // if (command === 'play')
+    // client.commands.get('play').execute(message,args[2])
+
+    // function play music
+    // const voice_channel = message.members.voice.channel;
+    // const server_queue = queue.get(message.guild.id)
+
+    // const connection = await voiceChannel.join();
+
+    if (!voiceChannel) return message.channel.send('You need to be in a voice channel to execute this command!');
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissions')
+    if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissions')
+    if (!args.length) return message.channel.send('You need to send the second argument')
+
+    const videoFinder = async (query) => {
+      const videoResult = await ytSearch(query);
+
+      return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
     }
-    if(message.content == `${config.prefix} toca`){
+
+    const video = await videoFinder(args.join(' '));
+
+    if (video) {
+      const stream = ytdl(video.url, { filter: 'audioonly' })
+      connection.play(stream, { seek: 0, volume: 1 })
+        .on('finish', () => {
+          voiceChannel.leave();
+        });
+
+      await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
+    } else {
+      message.channel.send('No video results found');
+    }
+
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :slight_smile:
+    console.error(e);
+  });
+})
+
+    //
+
+
+
+    //   client.channels.fetch(voice.channelID).then(channel => {
+    //     if (!channel) return console.error("The channel does not exist!");
+    //     channel.join().then(connection => {
+    //         // Yay, it worked!
+    //         console.log("Successfully connected.");
+    //     }).catch(e => {
+    //         // Oh no, it errored! Let's log it to console :slight_smile:
+    //         console.error(e);
+    //     });
+    // })
+
+
+
+    // const { voice } = message.member
+    // console.log(message.member.voice.channelId)
+
+    // if(!voice.channelId){
+    //   message.reply('You must be in a voice channel !')
+    //   return
+    // }
+
+    // else{
+
+    // }
+
+    if (message.content == `${config.prefix} toca`) {
       channel.leave();
     }
   }
--- play.js
+++ play.js
@@ -5,15 +5,20 @@ const queue = new Map();
 
 module.exports = {
   name: 'play',
-  aliases: ['skip', 'stop'],
-  cooldown: 0,
   description: 'zoe music bot',
-  async execute(message, args, cmd, client, Discord){
+  async execute(message, args){
     const voice_channel = message.members.voice.channel;
-    const permissions = voice_channel.permissionsFor(message.client.user);
     const server_queue = queue.get(message.guild.id)
+    return (args);
 
+    console.log(args);
     const connection = await voiceChannel.join();
+
+    if(!voiceChannel) return message.channel.send('You need to be in a voice channel to execute this command!');
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if(!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissions')
+    if(!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissions')
+    if(!args.length) return message.channel.send('You need to send the second argument')
 
     const videoFinder = async (query) => {
       const videoResult = await ytSearch(query);
@@ -21,7 +26,18 @@ module.exports = {
       return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
     }
 
-    const video = await videoFinder()
+    const video = await videoFinder(args.join(' '));
 
+    if(video){
+      const stream = ytdl(video.url, {filter: 'audioonly'})
+      connection.play(stream, {seek: 0, volume: 1})
+      .on('finish', () =>{
+        voiceChannel.leave();
+      });
+
+      await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
+    }else{
+      message.channel.send('No video results found');
+    }
   }
 }
